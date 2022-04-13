bac7bda1d10b1b919f3f6893e378a812c4905cdd
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2022-04-13 18:17:00 -0400

    Initial commit

---
+++ src/main/java/io/github/thepoultryman/buckethat/BucketHat.java
@@ -0,0 +1,12 @@
+package io.github.thepoultryman.buckethat;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BucketHat {
+    public static final Logger LOGGER = LoggerFactory.getLogger("BucketHat");
+
+    public static void main(String[] args) {
+        LOGGER.info("hey");
+    }
+}

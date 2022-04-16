272258998c38a683355ed1d8c0681b8dcd4d79eb
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2022-04-16 16:24:10 -0400

    Javadog

--- src/main/java/io/github/thepoultryman/buckethat/command/guild/GuildCommandReader.java
+++ src/main/java/io/github/thepoultryman/buckethat/command/guild/GuildCommandReader.java
@@ -23,6 +23,23 @@ public class GuildCommandReader {
         this.rootDir = rootDir;
     }
 
+    /**
+     * <p>Reads the guild command directory for guild commands to create.
+     * If the directory does not exist then a warning will be logged, and
+     * a fake ping command will be added. The directory will be read and
+     * each file will be read for the basic command data that it requires.</p>
+     * <p>Both Basic and Basic-Embed commands are read from this directory.
+     * Depending on the type of command, that is determined from the
+     * associated field in the JSON file, it will be added to a different
+     * map within {@link GuildCommandHelper}.</p>
+     * <p>When the directory is being iterated through, if there is an
+     * unexpected directory, a warning will be passed into the log.</p>
+     * <br>
+     * <b>Expected Directories:</b>
+     * <ul>
+     *     <li>embeds</li>
+     * </ul>
+     */
     public void readBasicCommands() {
         File directory = new File(this.rootDir + "\\data\\guild");
         if (directory.exists()) {
@@ -58,6 +75,12 @@ public class GuildCommandReader {
         }
     }
 
+    /**
+     * <p>Reads the embed command directory to create embed commands.
+     * If the directory does not exist, a warning will be logged. If
+     * the directory does exist, it will be iterated through to create
+     * embed commands.</p>
+     */
     public void readEmbedCommands() {
         File directory = new File(this.rootDir + "\\data\\guild\\embeds");
         if (directory.exists()) {

7aac23a36f158a8ad631cc46ac3e60ca1905838c
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2022-04-16 00:59:12 -0400

    Javadoc

--- src/main/java/io/github/thepoultryman/buckethat/command/guild/CommandListener.java
+++ src/main/java/io/github/thepoultryman/buckethat/command/guild/CommandListener.java
@@ -5,6 +5,23 @@ import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 
 public class CommandListener extends ListenerAdapter {
+
+    /**
+     * <p>When a slash command is made this method will be run. Using
+     * the slash command event provided by the super method, and Discord,
+     * the name of the command will be assigned to the {@code commandName}
+     * variable. This variable is then used with {@link GuildCommandHelper}
+     * to check what type of command was used. Once the type has been
+     * determined the appropriate method from {@link GuildCommandHelper}
+     * will be run so the correct reply can be sent to Discord.</p>
+     * <p>The command is checked by seeing if it is in any of the maps
+     * in {@link GuildCommandHelper}. If a command is not in the correct
+     * map, the bot will not respond, and Discord will provide an
+     * ephemeral message to the user of the command that reads, "The
+     * application did not respond"</p>
+     * @param event A slash command event passed by JDA, and Discord,
+     *              that is used to respond to a slash command.
+     */
     @Override
     public void onSlashCommandInteraction(@NotNull SlashCommandInteractionEvent event) {
         String commandName = event.getName();

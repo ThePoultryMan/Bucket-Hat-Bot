e699ed77ba1ea9b702acdc3347f40e4431944e4a
ThePoultryMan <66281330+ThePoultryMan@users.noreply.github.com>
2022-04-16 20:27:48 -0400

    Add options and subcommands methods

--- src/main/java/io/github/thepoultryman/buckethat/command/global/GlobalCommand.java
+++ src/main/java/io/github/thepoultryman/buckethat/command/global/GlobalCommand.java
@@ -2,19 +2,31 @@ package io.github.thepoultryman.buckethat.command.global;
 
 import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
+import net.dv8tion.jda.api.interactions.commands.build.SubcommandData;
 
 public class GlobalCommand {
     private final String name;
     private final String description;
+    private final SlashCommandData commandData;
 
     public GlobalCommand(String name, String description) {
         this.name = name;
         this.description = description;
+        this.commandData = Commands.slash(name, description);
     }
 
     public SlashCommandData getCommandData() {
-        return Commands.slash(name, description);
+        return this.commandData;
+    }
+
+    public void addOptions(OptionData... options) {
+        this.commandData.addOptions(options);
+    }
+
+    public void addSubcommands(SubcommandData... subcommands) {
+        this.commandData.addSubcommands(subcommands);
     }
 
     public String getName() {
